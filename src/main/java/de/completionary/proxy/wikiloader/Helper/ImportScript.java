6c0b97d3800cac69331f1c8ee16a0f001b75a581
Jonas <kunzejo@uni-mainz.de>
2014-07-18 17:51:22 +0200

    Formatted code

--- src/main/java/de/completionary/proxy/wikiloader/Helper/ImportScript.java
+++ src/main/java/de/completionary/proxy/wikiloader/Helper/ImportScript.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 import org.apache.commons.codec.binary.Base64;
 
-import de.completionary.proxy.elasticsearch.SuggestionField;
+import de.completionary.proxy.structs.SuggestionField;
 
 /**
  * @author Rene Pickhardt
@@ -24,106 +24,110 @@ import de.completionary.proxy.elasticsearch.SuggestionField;
  */
 public class ImportScript {
 
-	public static List<SuggestionField> loadServerWiki(
-			final int maxNumberOfElements) {
-		List<SuggestionField> fields = new ArrayList<SuggestionField>();
-		// articlswiththumbnail.tsv: ID \t Lemma \t image path
-		// Pagerank: wiki-pr.tsv ID \t prvalue
-		// path: /home/metalroot/
-		// imagepath:
-		// http://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Aki_Kaurism%C3%A4ki.jpg/92px-Aki_Kaurism%C3%A4ki.jpg
+    public static List<SuggestionField> loadServerWiki(
+            final int maxNumberOfElements) {
+        List<SuggestionField> fields = new ArrayList<SuggestionField>();
+        // articlswiththumbnail.tsv: ID \t Lemma \t image path
+        // Pagerank: wiki-pr.tsv ID \t prvalue
+        // path: /home/metalroot/
+        // imagepath:
+        // http://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Aki_Kaurism%C3%A4ki.jpg/92px-Aki_Kaurism%C3%A4ki.jpg
 
-		String path = "/var/lib/datasets/rawdata/commons/wiki-pr.tsv";
-		HashMap<Integer, Integer> prValues = new HashMap<Integer, Integer>();
-		// HashMap<String, Integer> prTitles = new HashMap<String, Integer>();
+        String path = "/var/lib/datasets/rawdata/commons/wiki-pr.tsv";
+        HashMap<Integer, Integer> prValues = new HashMap<Integer, Integer>();
+        // HashMap<String, Integer> prTitles = new HashMap<String, Integer>();
 
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
-			String line = null;
-			try {
-				while ((line = br.readLine()) != null) {
-					String[] values = line.split("\t");
-					Integer id = Integer.parseInt(values[0]);
-					Double pr = Double.parseDouble(values[1]);
-					double d = pr * 1000. * 1000. * 10000.;
-					prValues.put(id, (int) d);
-				}
-				System.out.println("Finished reading " + prValues.size()
-						+ " pagerank values");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (IOException e4) {
-			e4.printStackTrace();
-		}
+            String line = null;
+            try {
+                while ((line = br.readLine()) != null) {
+                    String[] values = line.split("\t");
+                    Integer id = Integer.parseInt(values[0]);
+                    Double pr = Double.parseDouble(values[1]);
+                    double d = pr * 1000. * 1000. * 10000.;
+                    prValues.put(id, (int) d);
+                }
+                System.out.println("Finished reading " + prValues.size()
+                        + " pagerank values");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (IOException e4) {
+            e4.printStackTrace();
+        }
 
-		String imagePath = "/var/lib/datasets/rawdata/commons/IMAGES/";
-		try (BufferedReader br = new BufferedReader(new FileReader(
-				"/var/lib/datasets/rawdata/commons/articlswiththumbnail.tsv"))) {
+        String imagePath = "/var/lib/datasets/rawdata/commons/IMAGES/";
+        try (BufferedReader br =
+                new BufferedReader(
+                        new FileReader(
+                                "/var/lib/datasets/rawdata/commons/articlswiththumbnail.tsv"))) {
 
-			String line = null;
-			while ((line = br.readLine()) != null) {
-				String[] values = line.split("\t");
-				if (values.length != 3)
-					continue;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split("\t");
+                if (values.length != 3)
+                    continue;
 
-				String id = values[0];
-				String title = values[1];
-				String key = title.replace(" ", "_");
-				String tmp = values[2];
-				values = tmp.replace(
-						"http://upload.wikimedia.org/wikipedia/commons/thumb/",
-						"").split("/");
-				if (values.length != 4)
-					continue;
-				String image = values[0] + "/" + values[1] + "/" + values[3];
+                String id = values[0];
+                String title = values[1];
+                String key = title.replace(" ", "_");
+                String tmp = values[2];
+                values =
+                        tmp.replace(
+                                "http://upload.wikimedia.org/wikipedia/commons/thumb/",
+                                "").split("/");
+                if (values.length != 4)
+                    continue;
+                String image = values[0] + "/" + values[1] + "/" + values[3];
 
-				File f = new File(imagePath + image);
-				if (!f.exists()) {
-					continue;
-				}
+                File f = new File(imagePath + image);
+                if (!f.exists()) {
+                    continue;
+                }
 
-				FileInputStream fileReader = null;
-				try {
-					fileReader = new FileInputStream(f);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-				byte[] cbuf = new byte[(int) f.length()];
-				try {
-					fileReader.read(cbuf, 0, (int) f.length());
-					fileReader.close();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-					continue;
-				}
-				byte[] base64EncodedImage = Base64.encodeBase64(cbuf);
-				String result = new String(base64EncodedImage);
-				result = "data:image/jpg;base64," + result;
+                FileInputStream fileReader = null;
+                try {
+                    fileReader = new FileInputStream(f);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                byte[] cbuf = new byte[(int) f.length()];
+                try {
+                    fileReader.read(cbuf, 0, (int) f.length());
+                    fileReader.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                    continue;
+                }
+                byte[] base64EncodedImage = Base64.encodeBase64(cbuf);
+                String result = new String(base64EncodedImage);
+                result = "data:image/jpg;base64," + result;
 
-				title = title.replace("_", " ").toLowerCase();
-				title = title.replace("-", " ");
-				Integer pr = prValues.get(Integer.parseInt(id));
-				if (pr != null && pr != null && key != null) {
-					values = title.split(" ");
-					SuggestionField field = new SuggestionField(key,
-							Arrays.asList(title), result, pr);
-					fields.add(field);
-					if (fields.size() == maxNumberOfElements) {
-						return fields;
-					}
-				}
-			}
-		} catch (FileNotFoundException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		} catch (IOException e3) {
-			// TODO Auto-generated catch block
-			e3.printStackTrace();
-		}
+                title = title.replace("_", " ").toLowerCase();
+                title = title.replace("-", " ");
+                Integer pr = prValues.get(Integer.parseInt(id));
+                if (pr != null && pr != null && key != null) {
+                    values = title.split(" ");
+                    SuggestionField field =
+                            new SuggestionField(key, Arrays.asList(title),
+                                    result, pr);
+                    fields.add(field);
+                    if (fields.size() == maxNumberOfElements) {
+                        return fields;
+                    }
+                }
+            }
+        } catch (FileNotFoundException e2) {
+            // TODO Auto-generated catch block
+            e2.printStackTrace();
+        } catch (IOException e3) {
+            // TODO Auto-generated catch block
+            e3.printStackTrace();
+        }
 
-		return fields;
-	}
+        return fields;
+    }
 }
--- src/main/java/de/completionary/proxy/wikiloader/Main.java
+++ src/main/java/de/completionary/proxy/wikiloader/Main.java
@@ -3,25 +3,33 @@ package de.completionary.proxy.wikiloader;
 import java.io.IOException;
 import java.util.List;
 
-import de.completionary.proxy.elasticsearch.SuggestionField;
 import de.completionary.proxy.elasticsearch.SuggestionIndex;
+import de.completionary.proxy.structs.SuggestionField;
 import de.completionary.proxy.wikiloader.Helper.ImportScript;
 
 public class Main {
 
-	public static void main(String[] args) {
-		List<SuggestionField> terms = ImportScript.loadServerWiki(10000);
+    public static void main(String[] args) {
+        List<SuggestionField> terms = ImportScript.loadServerWiki(10000);
 
-		SuggestionIndex client = new SuggestionIndex("index");
-		try {
-			long startTime = System.currentTimeMillis();
-			client.addTerms(terms);
-			long time = System.currentTimeMillis() - startTime;
-			System.out.println("Added " + terms.size() + " terms within "+time+" ms");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+        int bytesStored = 0;
+        for (SuggestionField field : terms) {
+            bytesStored += field.output.length() + field.payload.length();
+        }
+
+        SuggestionIndex client = new SuggestionIndex("index");
+        try {
+            long startTime = System.currentTimeMillis();
+            client.addTerms(terms);
+            long time = System.currentTimeMillis() - startTime;
+            System.out
+                    .println("Added " + terms.size() + " terms within " + time
+                            + " ms with about " + bytesStored / 1000
+                            + " kBytes");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
