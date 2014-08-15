3e971badc94e5680dcd17b3efd6037a6a7a5cf16
Jonas Kunze <kunze.jonas@gmail.com>
2014-08-15 23:53:41 +0200

    Switched back to bulk import as this is bugfixed now

--- src/main/java/de/completionary/proxy/wikiloader/Main.java
+++ src/main/java/de/completionary/proxy/wikiloader/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException,
             ExecutionException {
-        final List<SuggestionField> terms = ImportScript.loadServerWiki(1000);
+        final List<SuggestionField> terms = ImportScript.loadServerWiki(10000000);
 
         int bytesStored = 0;
         for (SuggestionField field : terms) {
@@ -32,53 +32,47 @@ public class Main {
             client = SuggestionIndex.getIndex("wikipediaindex");
 
             client.truncate();
-            long startTime = System.currentTimeMillis();
-
-            for (SuggestionField field : terms) {
-                final CountDownLatch lock = new CountDownLatch(1);
-                client.async_addSingleTerm(field.ID, field.input,
-                        field.outputField, field.payload, field.weight,
-                        new AsyncMethodCallback<Long>() {
-
-                            @Override
-                            public void onError(Exception e) {
-                                e.printStackTrace();
-                                lock.countDown();
-                            }
-
-                            @Override
-                            public void onComplete(Long time) {
-                                lock.countDown();
-                            }
-                        });
-                lock.await(2000, TimeUnit.MILLISECONDS);
-            }
+//            for (SuggestionField field : terms) {
+//                final CountDownLatch lock = new CountDownLatch(1);
+//                client.async_addSingleTerm(field.ID, field.input,
+//                        field.outputField, field.payload, field.weight,
+//                        new AsyncMethodCallback<Long>() {
+//
+//                            @Override
+//                            public void onError(Exception e) {
+//                                e.printStackTrace();
+//                                lock.countDown();
+//                            }
+//
+//                            @Override
+//                            public void onComplete(Long time) {
+//                                lock.countDown();
+//                            }
+//                        });
+//                lock.await(2000, TimeUnit.MILLISECONDS);
+//            }
             System.out.println("Added " + terms.size() + " terms with about "
                     + fBytesStored / 1000 + " kBytes");
 
-            //            
-            //            client.async_addTerms(terms, new AsyncMethodCallback<Long>() {
-            //
-            //                @Override
-            //                public void onError(Exception e) {
-            //                    e.printStackTrace();
-            //                }
-            //
-            //                @Override
-            //                public void onComplete(Long time) {
-            //                    System.out.println("Added " + terms.size()
-            //                            + " terms within " + time + " ms with about "
-            //                            + fBytesStored / 1000 + " kBytes");
-            //                }
-            //            });
+            client.async_addTerms(terms, new AsyncMethodCallback<Long>() {
 
-            client.waitForGreen();
+                @Override
+                public void onError(Exception e) {
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onComplete(Long time) {
+                    System.out.println("Added " + terms.size()
+                            + " terms within " + time + " ms with about "
+                            + fBytesStored / 1000 + " kBytes");
+                }
+            });
+
         } catch (InvalidIndexNameException | ServerDownException e1) {
             e1.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
 }
